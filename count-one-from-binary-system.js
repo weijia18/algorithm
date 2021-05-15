f873eb6a216598933e4dfb0d47747125bbe588f6
weijia18 <2658402573@qq.com>
2021-05-15 15:04:42 +0800

    求十进制整数二进制表示法中1的个数

---
+++ count-one-from-binary-system.js
@@ -0,0 +1,46 @@
+/**
+ * 1. 求出负数绝对值的原码
+ * 2. 从原码的最后一位数码位往前数，当遇到第一个1时停在此位置
+ * 3. 将第一个1前面的数码全部求反
+ */
+function countOne(n){
+    let oneNums = 0,
+        quotient = n,
+        remainder,
+        firstOneIndex = 0//负数绝对值从后数第一个1出现的位置
+    if( n >= 0){
+        while (quotient !== 0) {
+            remainder = quotient % 2
+            quotient = Math.floor(quotient/2)
+            if(remainder === 1){
+                oneNums++
+            }
+
+        }
+    }else{
+        quotient = -n
+        while (quotient !== 0) {
+            remainder = quotient % 2
+            quotient = Math.floor(quotient/2)
+            firstOneIndex++
+            if(remainder === 1){
+                break
+            }
+        }
+        quotient = -n
+        while (quotient !== 0) {
+            remainder = quotient % 2
+            quotient = Math.floor(quotient/2)
+            if(remainder === 1){
+                oneNums++
+            }
+        }
+        /**
+         * (32 - firstOneIndex) - (oneNums - 1) + 1
+         */
+        oneNums = (34 - firstOneIndex - oneNums)
+    }
+    return oneNums
+}
+
+console.log(countOne(process.argv[2]))
--- recursion/jump-steps.js
+++ recursion/jump-steps.js
@@ -2,6 +2,8 @@
  * n级台阶有getJumpWays(n)种跳法（每次跳两级或一级）
  * function getJumpWays(n){return getJumpWays(n-1) + getJumpWays(n-2)}
  * 
+ * 矩形覆盖问题也是类型的问题
+ * 
  */
 
 let memorizer = require('./memorizer.js')
