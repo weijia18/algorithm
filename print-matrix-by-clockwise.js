2b02980d88181237ec3b0ea07088ac0818160882
weijia18 <2658402573@qq.com>
2021-05-31 20:16:13 +0800

    顺时针打印矩阵

--- print-matrix-by-clockwise.js
+++ print-matrix-by-clockwise.js
@@ -1,3 +1,7 @@
+/**
+ * 矩阵从外到内顺时针打印矩阵
+ * 
+ */
 function printMatrixByClockwise(matrix) {
   let m = matrix.length
   let n = matrix[0].length
@@ -46,5 +50,13 @@ function matrixSlice(matrix) {
 }
 
 
-printMatrixByClockwise([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+//printMatrixByClockwise([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+let l = []
+for(let i = 0; i<10; i++){
+  l[i] = new Array()
+  for(let j =0; j<10; j++){
+    l[i][j] = (i)*10 + (j+1)
+  }
+}
+printMatrixByClockwise(l)
 console.log(list)
