3918091a38c070dc320f99fc7fb171c8a49645aa
weijia18 <2658402573@qq.com>
2021-06-19 11:22:47 +0800

    寻找等于给定值的连续正整数

--- math/find-series-count-s.js
+++ math/find-series-count-s.js
@@ -1,11 +1,18 @@
+/**
+ * 寻找等于给定值的连续正整数
+ * @param {*} s 
+ */
 function findSeries(s){
   let result = []
-  for(let n = 1; n < Math.ceil(s / 2); n++){
+  for(let n = 2; n < Math.ceil(s / 2); n++){
     let tmp = []
     if(n % 2 === 0){
       if((2*s) % n !== 0){
         continue
       }else{
+        if(Math.floor(s / n) - n / 2 + 1 < 1){
+          continue
+        }
         for(let j = Math.floor(s / n) - n / 2 + 1; j <= Math.floor(s / n) + n / 2; j++){
           tmp.push(j)
         }
@@ -14,6 +21,9 @@ function findSeries(s){
       if(s % n !== 0){
         continue
       }else{
+        if(s/n - Math.floor(n / 2) < 1){
+          continue
+        }
         for(let j = s/n - Math.floor(n / 2); j <= s/n + Math.floor(n / 2); j++){
           tmp.push(j)
         }
@@ -27,5 +37,7 @@ function findSeries(s){
 var mrandom = require('../utils/random.js')
 
 for(let i = 1; i < 2; i++){
-  console.log(findSeries(mrandom(1,1000)))
+  let tmp = mrandom(1,100000)
+  console.log(tmp)
+  console.log(findSeries(tmp))
 }
