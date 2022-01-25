ce63317ce9fcdce469c670209f92ade23cff07c1
weijia18 <2658402573@qq.com>
2022-01-25 18:12:57 +0800

    矩阵旋转

---
+++ array/rotateMatrix.js
@@ -0,0 +1,23 @@
+var rotate = function (matrix) {
+    let len = matrix.length
+    let coefficients = []
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            coefficients.push("" + i + j)
+        }
+    }
+    while (coefficients.length > 0) {
+        let coefficient = coefficients.pop()
+        let row = coefficient.split('')[0]
+        let col = coefficient.split('')[0]
+        let rowChange = col
+        let colChange = len - 1 - row
+        let coefficientChange = "" + rowChange + colChange
+        let tmp = matrix[row][col]
+        matrix[row][col] = matrix[rowChange][colChange]
+        matrix[rowChange][colChange] = tmp
+        let index = coefficients.indexOf(coefficientChange)
+        coefficients.splice(index, 1)
+    }
+    return matrix
+}
