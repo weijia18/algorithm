e471e1ea1318393e9c5a05209d6dd6b09380aff4
weijia18 <2658402573@qq.com>
2022-01-27 15:41:40 +0800

    update

--- tree/cengxu-order.js
+++ tree/cengxu-order.js
@@ -5,29 +5,25 @@
 // [[8],[6,10],[5,7,9,11]]
 
 
-
-function Print(pRoot)
-{
+//采用的是树的层序遍历
+function Print(pRoot) {
     // write code here
     let queue = []
-    let row = 0
-    let col = 0
     let res = []
     pRoot.row = 0
     queue.push(pRoot)
-    let p = queue.shift()
-    while(queue.length !== 0){
+    while (queue.length !== 0) {
         let p = queue.shift()
-        
-        if(!res[p.row]){
-           res[p.row] = new Array()
+
+        if (!res[p.row]) {
+            res[p.row] = new Array()
         }
         res[p.row].push(p.val)
-        if(p.left !== null){
+        if (p.left !== null) {
             p.left.row = p.row + 1
             queue.push(p.left)
         }
-        if(p.right !== null){
+        if (p.right !== null) {
             p.right.row = p.row + 1
             queue.push(p.right)
         }
@@ -35,21 +31,20 @@ function Print(pRoot)
     return res
 }
 
-//别人写的，用递归，把层深度传入进去
-function Print2(pRoot)
-{
+//别人写的，用递归，把层深度传入进去,采用的是树的中序遍历
+function Print2(pRoot) {
     // write code here
- const res = []
- const traversal = (root,depth) => {
-     if(root !== null){
-         if(!res[depth]){
-             res[depth] = []
-         }
-         traversal(root.left,depth+1)
-         res[depth].push(root.val)
-          traversal(root.right,depth+1)
-     }
- }
- traversal(pRoot,0)
- return res
+    const res = []
+    const traversal = (root, depth) => {
+        if (root !== null) {
+            if (!res[depth]) {
+                res[depth] = []
+            }
+            traversal(root.left, depth + 1)
+            res[depth].push(root.val)
+            traversal(root.right, depth + 1)
+        }
+    }
+    traversal(pRoot, 0)
+    return res
 }
