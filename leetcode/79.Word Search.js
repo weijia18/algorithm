d35c15d130d0dfdb3fa52aade4833e8beddee79b
weijia18 <2658402573@qq.com>
2022-02-17 14:50:25 +0800

    单词搜素

--- leetcode/79.Word Search.js
+++ leetcode/79.Word Search.js
@@ -6,16 +6,35 @@ var exist = function (board, word) {
     let colIndexs = []
     var backtrack = function (t) {
         if (t >= wLen) {
-
+            return true
         } else {
             for (let i = 0; i < rowLen; i++) {
                 for (let j = 0; j < colLen; j++) {
-                    if (rowIndexs.length === 0) {
+                    let l = rowIndexs.length
+                    if (board[i][j] !== word[l]) {
+                        continue
+                    }
+                    if (l === 0) {
                         rowIndexs.push(i)
                         colIndexs.push(j)
+                        backtrack(t + 1)
+                        rowIndexs.pop()
+                        colIndexs.pop()
+                    } else {
+                        let lastRowIndex = rowIndexs[l - 1]
+                        let lastColIndex = colIndexs[l - 1]
+                        if ((i - lastRowIndex === 0 && j - lastColIndex === 1) || (i - lastRowIndex === 1 && j - lastColIndex === 0)) {
+                            rowIndexs.push(i)
+                            colIndexs.push(j)
+                            backtrack(t + 1)
+                            rowIndexs.pop()
+                            colIndexs.pop()
+                        }
                     }
                 }
             }
         }
     }
+
+    return !!backtrack(0)
 }
